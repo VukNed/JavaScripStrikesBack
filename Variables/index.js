d059ac028e98d01679258185911799188bae9671
VukNed <nedovic.vuk@gmail.com>
2023-08-02 18:29:25 +0200

    basic objects

--- Variables/index.js
+++ Variables/index.js
@@ -1,12 +1,30 @@
-let name = 'Marko';
-console.log(name);
+// let name = 'Marko';
+// console.log(name);
 
-let firstName = 'Marko';
-let lastName = 'Markovic';
+// let firstName = 'Marko';
+// let lastName = 'Markovic';
 
-console.log(firstName + ' ' + lastName);
+// console.log(firstName + ' ' + lastName);
 
-const interestRate = 0.3;
-// interestRate = 1;
-// empty comment to tes git commit
-console.log(interestRate);
+// const interestRate = 0.3;
+// // interestRate = 1;
+// // empty comment to tes git commit
+// console.log(interestRate);
+
+// let name = 'Marko';
+// let age = 30;
+// let isApproved = false;
+// let firstName = undefined;
+// let selectedColor = null;
+
+// let name = 'Marko';
+// let age = 30;
+let person = {
+    name: 'Marko',
+    age: 30
+};
+
+person.name = 'Nikola';
+console.log(person.name);
+
+// person['name'] = 'Martin';
